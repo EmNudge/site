86b3434bdeb1e7be60e6061361715004ea95f264
EmNudge <24513691+EmNudge@users.noreply.github.com>
2022-02-12 20:07:28 -0500

    add picture zoom

---
+++ src/components/PictureZoom.svelte
@@ -0,0 +1,51 @@
+<script lang="ts">
+    import { onMount } from "svelte";
+    import { fade } from "svelte/transition";
+
+    let clickedImage = '';
+
+    onMount(() => {
+        const articleEl = document.querySelector('article');
+        articleEl.classList.add("js-enabled");
+        articleEl.addEventListener("click", (e) => {
+            if (!(e.target instanceof HTMLImageElement)) return;
+            if (!["P", "FIGURE"].includes(e.target.parentElement.tagName))
+                return;
+            // if we clicked an image that is a direct descendant of a paragraph element
+            clickedImage = e.target.src;
+        });
+    });
+
+    function maybeCloseImage(e) {
+        if (e.target instanceof HTMLImageElement) return;
+        clickedImage = '';
+    }
+</script>
+
+{#if clickedImage}
+    <div
+        class="overlay-image"
+        transition:fade={{ duration: 100 }}
+        on:click={maybeCloseImage}
+    >
+        <img src={clickedImage} alt="large version of thing you clicked" />
+    </div>
+{/if}
+
+<style>
+    .overlay-image {
+        display: flex;
+        justify-content: center;
+        align-items: center;
+        position: fixed;
+        top: 0;
+        left: 0;
+        width: 100%;
+        height: 100%;
+        background: #000a;
+    }
+    .overlay-image img {
+        max-height: 80%;
+        max-width: 80%;
+    }
+</style>
