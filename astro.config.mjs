af7e66af751ce92ba749716d8f736456e25c80cf
EmNudge <24513691+EmNudge@users.noreply.github.com>
2022-02-13 15:18:28 -0500

    add actual youtube API

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,3 +1,11 @@
+import dotenv from "dotenv"
+import fs from "fs/promises"
+
+const env = dotenv.parse(await fs.readFile(".env"))
+const exposedKeys = ['YOUTUBE_API_KEY', 'YOUTUBE_CHANNEL_ID']
+const envDefinitions = Object.fromEntries(exposedKeys.map(key => [`process.env.${key}`, `"${env[key]}"`]))
+
+
 // @type-check enabled!
 // VSCode and other TypeScript-enabled text editors will provide auto-completion,
 // helpful tooltips, and warnings if your exported object is invalid.
@@ -8,5 +16,10 @@ export default /** @type {import('astro').AstroUserConfig} */ ({
     renderers: ['@astrojs/renderer-svelte'],
     buildOptions: {
         site: 'https://kipperman.co',
-    }
+    },
+    vite: {
+        define: {
+            ...envDefinitions
+        }
+    },
 });
--- src/data/videos.ts
+++ src/data/videos.ts
@@ -1,22 +1,42 @@
-export const videos = [
-    {
-        "id": "IUmC_dC87oc",
-        "title": "Rate Limiting - From Scratch Episode 3",
-        "img": "https://i.ytimg.com/vi/IUmC_dC87oc/mqdefault.jpg"
-    },
-    {
-        "id": "lebu6I-qJrg",
-        "title": "Understanding The Standard For Loop",
-        "img": "https://i.ytimg.com/vi/lebu6I-qJrg/mqdefault.jpg"
-    },
-    {
-        "id": "7DAG74o3p3o",
-        "title": "World Champion Typist 320wpm - How I did it",
-        "img": "https://i.ytimg.com/vi/7DAG74o3p3o/mqdefault.jpg"
-    },
-    {
-        "id": "V3vZSO4zpFs",
-        "title": "Understanding Loose Equality In JavaScript",
-        "img": "https://i.ytimg.com/vi/V3vZSO4zpFs/mqdefault.jpg"
-    }
-];
+const key = process.env.YOUTUBE_API_KEY
+const channel = process.env.YOUTUBE_CHANNEL_ID;
+const MAX_RESULTS = 4;
+
+const BASE_URL = 'https://www.googleapis.com/youtube/v3/search';
+function getYoutubeUrl() {
+    const params = {
+        key: key,
+        channelId: channel,
+        part: 'snippet,id',
+        order: 'date',
+        maxResults: MAX_RESULTS
+    };
+    const paramsStr = Object.entries(params).map(([k, v]) => `${k}=${v}`).join('&')
+
+    return `${BASE_URL}?${paramsStr}`;
+}
+
+export interface Video {
+    title: string;
+    id: string;
+    link: string;
+    img: string;
+}
+
+let videos: Video[] = null;
+export async function getVideos() {
+    if (videos) return videos;
+
+    const json = await fetch(getYoutubeUrl()).then(res => res.json());
+
+    videos = json.items.map(({ snippet, id: snippetId }) => {
+        const { title, thumbnails } = snippet;
+        const id = snippetId.videoId;
+        const link = `https://youtube.com/watch?v=${id}`;
+        const img = thumbnails.medium.url;
+
+        return { title, id, link, img } as Video;
+    });
+
+    return videos;
+}
