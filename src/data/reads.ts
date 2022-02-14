b041f43093334f756d500a71cfb3d64b0ae73f3b
EmNudge <24513691+EmNudge@users.noreply.github.com>
2022-02-13 21:04:31 -0500

    fix no links on stub

--- src/data/reads.ts
+++ src/data/reads.ts
@@ -14,15 +14,27 @@ const getHeaders = (line: string) => {
     return headers;
 }
 
-const csvParse = (csv: string) => {
+interface CsvRow {
+    title: string;
+    author: string;
+    description: string;
+    createdAt: string;
+    readAt: string;
+    link: string;
+}
+
+const csvParse = (csv: string): CsvRow[] => {
     const lines = csv.split('\n');
     const headers = getHeaders(lines[0]);
 
-    return lines.slice(1).map(line => {
-        const matches = [...line.matchAll(PARSE_LINE)].map(m => m[2]);
-        const entries = matches.map((match, i) => [headers[i], match]);
-        return Object.fromEntries(entries);
-    });
+    return lines
+        .slice(1)
+        .map(line => {
+            const matches = [...line.matchAll(PARSE_LINE)].map(m => m[2]);
+            const entries = matches.map((match, i) => [headers[i], match]);
+            return Object.fromEntries(entries);
+        })
+        .sort((a, b) => +new Date(b.readAt) - +new Date(a.readAt));
 }
 
-export const reads = csvParse(csvData);
+export const reads: CsvRow[] = csvParse(csvData);
