efafdc2a6934175f7ca23320d5e46d1a2a55fc1e
EmNudge <24513691+EmNudge@users.noreply.github.com>
2023-03-22 17:24:47 -0700

    add projects

---
+++ src/data/projects.ts
@@ -0,0 +1,26 @@
+export const projects = [
+  {
+    name: "Unicode Lookup",
+    description: "Look up unicode tools in-browser and client-side",
+    github: "https://github.com/EmNudge/unicode-lookup",
+    url: "https://unicode.emnudge.dev/",
+  },
+  {
+    name: "Abstract Equality Stepper",
+    description: "Walk through the abstract equality spec, step by step",
+    url: "https://abs-eq.emnudge.dev/",
+    github: "https://github.com/EmNudge/Abstract-Equality-Guide",
+  },
+  {
+    name: "Syntax Tree Generator",
+    description: "Generate syntax trees using text",
+    url: "https://syntax-tree.netlify.app/",
+    github: "https://github.com/EmNudge/Syntax-Tree-Generator",
+  },
+  {
+    name: "Spectrogram Replicator",
+    description: "Draw sine waves, generate spectrograms, have fun",
+    url: "http://spectrogram.emnudge.dev/",
+    github: "https://github.com/EmNudge/Spectrogram-Replicator",
+  },
+];
