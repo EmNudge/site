71302241e96c800b920fc47cdf4ef94c7b90eb15
EmNudge <24513691+EmNudge@users.noreply.github.com>
2024-04-14 09:16:51 -0700

    add expressive code

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,41 +1,57 @@
 import { defineConfig } from "astro/config";
 import svelte from "@astrojs/svelte";
 import mdx from "@astrojs/mdx";
-
 import rehypeSlug from "rehype-slug";
 import rehypeAutolinkHeadings from "rehype-autolink-headings";
 import rehypeToc from "@jsdevtools/rehype-toc";
+import expressiveCode from "astro-expressive-code";
 
+// https://astro.build/config
 export default defineConfig({
-    integrations: [
-        svelte(),
-        mdx(),
+  integrations: [svelte(), expressiveCode(), mdx()],
+  markdown: {
+    syntaxHighlight: false,
+    rehypePlugins: [
+      rehypeSlug,
+      [
+        rehypeToc,
+        {
+          customizeTOC(node) {
+            node.tagName = "details";
+            const summary = {
+              type: "element",
+              tagName: "summary",
+              children: [
+                {
+                  type: "text",
+                  value: "Table Of Contents",
+                },
+              ],
+            };
+            node.children.unshift(summary);
+            return node;
+          },
+        },
+      ],
+      [
+        rehypeAutolinkHeadings,
+        {
+          behavior: "prepend",
+          content: {
+            type: "element",
+            tagName: "span",
+            properties: {
+              className: "header-link",
+            },
+            children: [
+              {
+                type: "text",
+                value: "#",
+              },
+            ],
+          },
+        },
+      ],
     ],
-    markdown: {
-        syntaxHighlight: false,
-        rehypePlugins: [
-            rehypeSlug,
-            [rehypeToc, {
-                customizeTOC(node) {
-                    node.tagName = "details";
-                    const summary = {
-                        type: "element",
-                        tagName: "summary",
-                        children: [{ type: "text", value: "Table Of Contents" }],
-                    };
-                    node.children.unshift(summary);
-                    return node;
-                },
-            }],
-            [rehypeAutolinkHeadings, {
-                behavior: "prepend",
-                content: {
-                    type: "element",
-                    tagName: "span",
-                    properties: { className: "header-link" },
-                    children: [{ type: "text", value: "#" }],
-                },
-            }],
-        ],
-    },
+  },
 });
